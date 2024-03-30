4a5259bff25030e63ab6c2b5bcd2206b8f7d4361
maung-ye-ko <fatherloveson@gmail.com>
2024-03-30 12:29:00 +0630

    making folder available

---
+++ UI/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
